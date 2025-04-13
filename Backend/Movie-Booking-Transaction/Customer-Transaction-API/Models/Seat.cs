a73e22c4c5e51565d10665d082cf668439cd9451
Mukeshmathankar <mukeshmathankar3@gmail.com>
2025-04-13 20:44:05 +0530

    Tansaction using .net web api

---
+++ Backend/Movie-Booking-Transaction/Customer-Transaction-API/Models/Seat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Customer_Transaction_API.Models;
+
+public partial class Seat
+{
+    public int SeatId { get; set; }
+
+    public int ScreenId { get; set; }
+
+    public string? Row { get; set; }
+
+    public int? Column { get; set; }
+
+    public bool? IsAvailable { get; set; }
+
+    public virtual Screen Screen { get; set; } = null!;
+}
